5dae8a6f3c44626b589b97e490b3832a51a82658
satyam2301 <satyamj.2301@gmail.com>
2023-12-06 14:06:56 +0530

    added user model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { ServerConfig, LoggerConfig } = require('./config');
+const { ServerConfig } = require('./config');
 
 const app = express();
 const apiRoutes = require('./routes');
 
-// app.get(path,middleware (optional),controller) same for app.use();
-// Below apiRoutes will be of app.get(path,controller) and this path will be attach next to the current path
-// E.g : app.use('/about',controller) ->from apiRoutes , So finally it will be as app.use('/api/about',controller)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
