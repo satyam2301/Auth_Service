6d8b8fe9d67b3c702302b5e0e4d018a865abda66
satyam2301 <satyamj.2301@gmail.com>
2023-12-14 15:12:11 +0530

    added jwt expiry error handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -69,6 +69,9 @@ async function isAuthenticated(token) {
     if (error.name == 'JsonWebTokenError') {
       throw new AppError('Invalid JWT token.', StatusCodes.BAD_REQUEST);
     }
+    if (error.name == 'TokenExpiredError') {
+      throw new AppError('JWT token Expired.', StatusCodes.BAD_REQUEST);
+    }
     console.log(error);
     throw new AppError(
       'Something went wrong.',
